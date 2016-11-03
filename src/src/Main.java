3bd6378b0c60954863cbad31f748886cbee28050
AndreyDelany <dielanie@freenet.de>
2016-11-03 17:07:54 +0100

    added Class that encrypt one character with vigenere

--- src/src/Main.java
+++ src/src/Main.java
@@ -3,8 +3,8 @@ package src;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(VigenereCharacterEncryption.encryptOneCharacter("b", "y"));
+		
 	}
 
 }
